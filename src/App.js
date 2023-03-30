44a6356d30cd9a0af59c6f3bc6f7af3a8ae39987
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-03-30 10:15:13 +0100

    Add aside

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import React from 'react-dom';
+import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SideBar from './components/aside/sideBar';
-import LifeStyle from './components/pages/lifestyle';
+import SideBar from './components/sidebar/SideBar';
 
 function App() {
   return (
-    <div>
-      <Router>
+    <div className="app">
+      <div className="d-flex flex-row">
         <SideBar />
-        <Routes>
-          <Route path="/lifestyle" exact element={<LifeStyle />} />
-        </Routes>
-      </Router>
+      </div>
     </div>
   );
 }
